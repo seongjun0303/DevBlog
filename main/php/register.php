a1036f913aa420ab0e6ce2cbaecbf329e7694a94
Seong Cho <seongcho9803@gmail.com>
2020-07-14 21:16:10 -0400

    add fields to users

--- main/php/register.php
+++ main/php/register.php
@@ -7,7 +7,7 @@ $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
 $firstname = $lastname = $address = $fullname = $cardnumber = $expiration = $cvc = "";
 $firstname_err = $lastname_err = $address_err = $fullname_err = $cardnumber_err = $expiration_err = $cvc_err = "";
-
+$email = $email_err = "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -120,27 +120,44 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $cvc = trim($_POST["cvc"]);
     }
 
+    // Validate email
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter email";
+    }
+    else{
+        $email = trim($_POST["email"]);
+    }
+
 
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)&& empty($address_err)&& empty($firstname_err)&& empty($lastname_err)&& empty($fullname_err)&& empty($expiration_err)&& empty($cardnumber_err)&& empty($cvc_err)){
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)&& empty($address_err)&& empty($firstname_err)&& empty($lastname_err)&& empty($fullname_err)&& empty($expiration_err)&& empty($cardnumber_err)&& empty($cvc_err)&&empty($email_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (username, password, firstname, lastname, address, fullname, cardnumber, expiration, cvc, email) VALUES (?, ?,?,?,?,?,?,?,?,?)";
          
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            mysqli_stmt_bind_param($stmt, "sssssssiis", $param_username, $param_password, $param_firstname, $param_lastname, $param_address, $param_fullname, $param_cardnumber, $param_expiration, $param_cvc, $param_email);
             
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_firstname = $firstname;
+            $param_lastname = $lastname;
+            $param_address = $address;
+            $param_fullname = $fullname;
+            $param_expiration = $expiration;
+            $param_cardnumber = password_hash($cardnumber, PASSWORD_DEFAULT);
+            $param_cvc = $cvc;
+            $param_email = $email;
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Redirect to login page
                 header("location: login.php");
             } else{
-                echo "Something went wrong. Please try again later.";
+                echo "Something went wrong. Please try again later.\n";
+                echo mysqli_stmt_error($stmt);
             }
 
             // Close statement
@@ -183,6 +200,47 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <label>Confirm Password</label>
                 <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+                <label>Email</label>
+                <input type="text" name="email" class="form-control" value="<?php echo $email; ?>">
+                <span class="help-block"><?php echo $email_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($firstname_err)) ? 'has-error' : ''; ?>">
+                <label>Firstname</label>
+                <input type="text" name="firstname" class="form-control" value="<?php echo $firstname; ?>">
+                <span class="help-block"><?php echo $firstname_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($lastname_err)) ? 'has-error' : ''; ?>">
+                <label>Lastname</label>
+                <input type="text" name="lastname" class="form-control" value="<?php echo $lastname; ?>">
+                <span class="help-block"><?php echo $lastname_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($address_err)) ? 'has-error' : ''; ?>">
+                <label>Address</label>
+                <input type="text" name="address" class="form-control" value="<?php echo $address; ?>">
+                <span class="help-block"><?php echo $address_err; ?></span>
+            </div>
+            <p>Payment info</p>
+            <div class="form-group <?php echo (!empty($fullname_err)) ? 'has-error' : ''; ?>">
+                <label>Full name as it appears on card</label>
+                <input type="text" name="fullname" class="form-control" value="<?php echo $fullname; ?>">
+                <span class="help-block"><?php echo $fullname_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($cardnumber_err)) ? 'has-error' : ''; ?>">
+                <label>Card number</label>
+                <input type="password" name="cardnumber" class="form-control" value="<?php echo $cardnumber; ?>">
+                <span class="help-block"><?php echo $cardnumber_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($expiration_err)) ? 'has-error' : ''; ?>">
+                <label>Expiration Date (enter 4 digits)</label>
+                <input type="number" name="expiration" class="form-control" value="<?php echo $expiration; ?>">
+                <span class="help-block"><?php echo $expiration_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($cvc_err)) ? 'has-error' : ''; ?>">
+                <label>CVC</label>
+                <input type="number" name="cvc" class="form-control" value="<?php echo $cvc; ?>">
+                <span class="help-block"><?php echo $cvc_err; ?></span>
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
